e18163f0def749b0a0b5e22ee6a8ecd7026d0ace
Dimas Julisa Taniawan <dimas@41studio.com>
2016-03-11 15:03:27 +0700

    change to rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+begin
+  require 'rails'
+rescue LoadError
+end
+
+require 'bundler/setup'
+Bundler.require
+
+require 'capybara/rspec'
