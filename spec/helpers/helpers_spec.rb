43783ace2b3e00dee41231bea7c2eee2353c401b
Dimas Julisa Taniawan <dimas@41studio.com>
2016-03-14 13:23:00 +0700

    add rspec for helpers

--- spec/helpers/helpers_spec.rb
+++ spec/helpers/helpers_spec.rb
@@ -4,8 +4,34 @@ describe 'AssetsAsync::Helpers' do
   describe "#include_assets_async" do
     subject { helper.include_assets_async }
 
-    it "should minified" do
+    it "should minified async js" do
       should_not match(/\n/)
+    end
+  end
+
+  describe "#load_css" do
+    css = 'application.css'
+    subject { helper.load_css(css) }
+
+    it "should have loadCSSAsync function" do
+      should match('loadCSSAsync')
+    end
+
+    it "should have name with included as param" do
+      should match(css)
+    end
+  end
+
+  describe "#load_js" do
+    js = 'application.js'
+    subject { helper.load_js(js) }
+
+    it "should have loadJSAsync function" do
+      should match('loadJSAsync')
+    end
+
+    it "should have name with included as param" do
+      should match(js)
     end
   end
 end
